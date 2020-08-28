e551c910fd6c30c9298ab5cdb50194f930e1d8c5
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-28 18:09:17 -0400

    pushed FEATURES down one more level

--- src/Selector.js
+++ src/Selector.js
@@ -3,14 +3,13 @@ import React from 'react';
 import SelectorHeader from './SelectorHeader';
 import SelectorFeatures from './SelectorFeatures';
 
-export default function Selector({ selected, updateFeature, FEATURES }) {
+export default function Selector({ selected, updateFeature }) {
   return (
     <form className="main__form">
       <SelectorHeader />
       <SelectorFeatures 
         selected={selected}
         updateFeature={updateFeature}
-        FEATURES={FEATURES}
       />
     </form>
   )
