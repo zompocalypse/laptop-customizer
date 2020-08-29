8b8d27e55060277157632f842f9860e65c89ffd9
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-28 22:45:54 -0400

    Updates

---
+++ precache-manifest.e78a75b8d06f792d5fdc58ff0d28769d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15238ade9e5f29a45449cdb3ec36efd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23579ce86cc625c8617c",
+    "url": "/static/css/main.42a420bb.chunk.css"
+  },
+  {
+    "revision": "91be17f00bf027269ee1",
+    "url": "/static/js/2.a3f18fbf.chunk.js"
+  },
+  {
+    "revision": "23579ce86cc625c8617c",
+    "url": "/static/js/main.4fd0d2c1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
